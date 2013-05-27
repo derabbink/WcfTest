6e6579b6bfa03f272b5f203a445310e130eb24a3
Maarten Abbink <m.abbink@fugro.nl>
2013-05-27 15:07:00 +0200

    added added svcutil generated code

---
+++ Client.Contracts/Channels/IMepRequestReplyGreeterChannel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WcfTest.Client.Contracts.Channels
+{
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.ServiceModel", "4.0.0.0")]
+    public interface IMepRequestReplyGreeterChannel : IMepRequestReplyGreeter, System.ServiceModel.IClientChannel
+    {
+    }
+}
---
+++ Client.Contracts/Clients/MepDuplexGreeterClient.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WcfTest.Client.Contracts.Clients
+{
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.ServiceModel", "4.0.0.0")]
+    public partial class MepDuplexGreeterClient : System.ServiceModel.DuplexClientBase<IMepDuplexGreeter>, IMepDuplexGreeter
+    {
+
+        public MepDuplexGreeterClient(System.ServiceModel.InstanceContext callbackInstance) :
+            base(callbackInstance)
+        {
+        }
+
+        public MepDuplexGreeterClient(System.ServiceModel.InstanceContext callbackInstance, string endpointConfigurationName) :
+            base(callbackInstance, endpointConfigurationName)
+        {
+        }
+
+        public MepDuplexGreeterClient(System.ServiceModel.InstanceContext callbackInstance, string endpointConfigurationName, string remoteAddress) :
+            base(callbackInstance, endpointConfigurationName, remoteAddress)
+        {
+        }
+
+        public MepDuplexGreeterClient(System.ServiceModel.InstanceContext callbackInstance, string endpointConfigurationName, System.ServiceModel.EndpointAddress remoteAddress) :
+            base(callbackInstance, endpointConfigurationName, remoteAddress)
+        {
+        }
+
+        public MepDuplexGreeterClient(System.ServiceModel.InstanceContext callbackInstance, System.ServiceModel.Channels.Binding binding, System.ServiceModel.EndpointAddress remoteAddress) :
+            base(callbackInstance, binding, remoteAddress)
+        {
+        }
+
+        public void SayHelloDuplex()
+        {
+            base.Channel.SayHelloDuplex();
+        }
+
+        public void ExchangeGreetingsDuplex(string name)
+        {
+            base.Channel.ExchangeGreetingsDuplex(name);
+        }
+
+        public void CauseErrorDuplex()
+        {
+            base.Channel.CauseErrorDuplex();
+        }
+    }
+}
